5581ea321839df66bfe1132eca9f51989051bc23
psnehal <snehal@ccmb-sartor893.bicc.med.umich.edu>
2014-07-25 15:50:56 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	bioinf_project/templates/base.html

---
+++ bioinf_project/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.views.generic.base import View
+from django.views.generic.edit import FormView
+# Create your views here.
+
+class Login(FormView):
+    form_class = AuthenticationForm
+    template_name = "users/login.html"
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return redirect('index')
+
+class Logout(View):
+    pass 
