66fb46fca27e355f528f920da7149c4690188a75
shawn <troublezhang@gmail.com>
2014-11-20 02:08:33 -0500

    wiki title

--- bioinf_project/apps/wiki/forms.py
+++ bioinf_project/apps/wiki/forms.py
@@ -1,5 +1,6 @@
 # forms.py
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Page
 from tags.models import Tag
@@ -7,8 +8,13 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Fieldset, Div, Submit, ButtonHolder, HTML
 
 
+def validate_title(name):
+    if name.find("_") >= 0:
+        raise ValidationError("'_' is not allowed in the wiki title")
+        
+        
 class PageForm(forms.ModelForm):
-    title = forms.CharField()
+    title = forms.CharField(validators=[validate_title],help_text="title of the wiki")
     tags= forms.ModelMultipleChoiceField(label="Tags", queryset=Tag.objects.all(), required=False)
     content = forms.CharField(widget=forms.Textarea)
 
@@ -42,7 +48,7 @@ PageForm.base_fields['tags'].help_text = 'Please type your tags'
 
 class PageRevisionForm(forms.ModelForm):
 
-    title = forms.CharField()
+    title = forms.CharField(validators=[validate_title],help_text="title of the wiki")
     tags= forms.ModelMultipleChoiceField(label="Tags", queryset=Tag.objects.all(),required=False)
     content = forms.CharField(widget=forms.Textarea)
     summary = forms.CharField()
