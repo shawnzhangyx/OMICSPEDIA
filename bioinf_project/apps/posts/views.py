bfa2ed8db4d8c6ca6333243d1259a72438eec45a
shawn <troublezhang@gmail.com>
2015-03-23 22:12:49 -0400

    search results view

--- bioinf_project/apps/posts/views.py
+++ bioinf_project/apps/posts/views.py
@@ -26,7 +26,7 @@ class IndexView(ListView):
     def get_queryset(self):
         tab = self.request.GET.get('tab')
         sort = self.request.GET.get('sort')
-        dict = {'Votes':'-vote_count','Replies':'-reply_count', 'Bookmarks':'bookmark_count','Views':'-view_count'}
+        dict = {'Votes':'-vote_count','Replies':'-reply_count', 'Bookmarks':'bookmark_count','Views':'-view_count', 'Created':'-created'}
         if sort in dict:
             sort_by = dict[sort]
         else: 
