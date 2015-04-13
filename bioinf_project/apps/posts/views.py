12cf29a26cddb9a40020776c95179532c9702771
shawn <troublezhang@gmail.com>
2015-04-13 14:56:55 -0400

    post bookmark bug fix

--- bioinf_project/apps/posts/views.py
+++ bioinf_project/apps/posts/views.py
@@ -174,8 +174,9 @@ class ReplyPostNew(CreateView):
         new_revision.save()
         form.instance.current_revision=new_revision
         content_type = ContentType.objects.get_for_model(MainPost)
-        new_bookmark = Bookmark.objects.get_or_create(reader = self.request.user, content_type = content_type, object_id = form.instance.mainpost.id)
-        form.instance.mainpost.bookmark_count += 1
+        new_bookmark,created = Bookmark.objects.get_or_create(reader = self.request.user, content_type = content_type, object_id = form.instance.mainpost.id)
+        if created:
+            form.instance.mainpost.bookmark_count += 1
         return super(ReplyPostNew, self).form_valid(form)
 
 class ReplyPostEdit(UpdateView):
